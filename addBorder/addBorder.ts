d696e8e3bdf43a2c51a59910f9b568e2f6cef37d
beekalam <beekalam@gmail.com>
2019-02-28 08:52:57 +0330

    add border

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,11 @@
 function addBorder(picture: string[]): string[] {
-
+    let line = "*".repeat(picture[0].length + 2);
+    let ret:string[] = [];
+    ret.push(line);
+    picture.forEach((v) => ret.push("*" + v + "*") );
+    ret.push(line);
+    return ret;
+    
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
