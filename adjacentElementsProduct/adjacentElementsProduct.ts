1db0374dacd1447b57200a996d84792d74018c9f
beekalam <beekalam@gmail.com>
2019-03-01 07:46:17 +0330

    adjacentElementsProduct

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,9 @@
 function adjacentElementsProduct(inputArray: number[]): number {
-
+    let maxProduct:number = 0;
+    for(let i = 1; i < inputArray.length; i++){
+        maxProduct = Math.max(maxProduct, inputArray[i] * inputArray[i-1]);
+    }
+    return maxProduct;
 }
 
 console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
