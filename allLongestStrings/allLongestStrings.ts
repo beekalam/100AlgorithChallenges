88e500a2f944567920f4cd78056604c2c1d38aba
beekalam <beekalam@gmail.com>
2019-03-02 20:52:13 +0330

    alllongeststrings

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,15 @@
 function allLongestStrings(inputArray: string[]): string[] {
-
+    let max:number = 0;
+    for(let item of  inputArray){
+        max = Math.max(item.length,max);
+    }
+    let ret:string[] = [];
+    for(let item of inputArray){
+        if(item.length == max){
+            ret.push(item);
+        }
+    }
+    return ret;
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
