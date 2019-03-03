66ab63b8fdacab9283e0c59e7467931d0c07b228
beekalam <beekalam@gmail.com>
2019-03-03 13:22:16 +0330

    alphabetsequence

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,5 +1,30 @@
 function alphabetSubsequence(s: string): boolean {
+    
+    for(let i = 1;i < s.length; i++){
+        if (s.charCodeAt(i) <= s.charCodeAt(i-1)) return false;
+    }
+    return true;
+}
 
+function alphabetSubsequence2(s: string): boolean {
+    
+    const chars:string[] = s.split('');
+    const charValues:number[] = [];
+
+    chars.forEach((char:string) =>{
+        charValues.push(char.charCodeAt(0));
+    });
+
+    if(new Set(charValues).size !== charValues.length){
+        return false;
+    }
+
+    for(let i = 0; i <charValues.length - 1; i++){
+        if(charValues[i] >= charValues[i+1]){
+            return false;
+        }
+    }
+    return true;
 }
 
 console.log(alphabetSubsequence('zab'))
