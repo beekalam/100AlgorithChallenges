908eee737124516ba4a0093f78777654b42074d7
beekalam <beekalam@gmail.com>
2019-03-03 11:49:05 +0330

    almostincreasingsequence

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,12 @@
 function almostIncreasingSequence(sequence: number[]): boolean {
-
+    let cnt:number = 0;
+    for(let i = 1; i < sequence.length; i++){
+        if(sequence[i] < sequence[i-1]){
+            cnt++;
+        }
+    }
+   return cnt <=  1;
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+console.log(almostIncreasingSequence([1, 3, 2, 1])) 
+console.log(almostIncreasingSequence([1, 3, 2])) 
