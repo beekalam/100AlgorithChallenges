1895c8fa96d787521e99eba0942cc72f98631290
beekalam <beekalam@gmail.com>
2019-03-04 07:13:58 +0330

    alphabeticshifts

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,13 @@
 function alphabeticShift(inputString: string): string {
+    let input = inputString.split('');
+    console.log(input);
+    let ret = "";
+    input.forEach((v:string) => {
+        if(v == 'z') ret += 'a';
+        else  ret += String.fromCharCode(v.charCodeAt(0)+1);
+    });
 
+    return ret;
 }
 
 console.log(alphabeticShift('crazy'));
