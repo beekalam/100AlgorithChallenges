0bcd4e07c98b6e638e9b5b95a1d0b94d81f80b50
beekalam <beekalam@gmail.com>
2019-03-07 08:08:50 +0330

    aresimilar

--- areSimilar/areSimilar.ts
+++ areSimilar/areSimilar.ts
@@ -1,6 +1,11 @@
 function areSimilar(a: number[], b: number[]): boolean {
-   
-}
+    let cnt = 0;
+    for(let i=0;i <a.length; i++){
+        if(a[i] != b[i]) cnt++;
+    }
+    
+    return cnt > 2 ? false:true;
+}   
 
 console.log(areSimilar([1, 2, 3], [1, 2, 3]));
 console.log(areSimilar([1, 2, 3], [2, 1, 3]));
