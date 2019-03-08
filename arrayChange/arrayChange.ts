2e426a90c336fc55b0128541ebbbec2db31640eb
beekalam <beekalam@gmail.com>
2019-03-08 11:09:39 +0330

    arraychange

--- arrayChange/arrayChange.ts
+++ arrayChange/arrayChange.ts
@@ -1,5 +1,13 @@
 function arrayChange(inputArray: number[]): number {
- 
+    let cnt:number = 0;
+    for(let i = 1; i < inputArray.length; i++){
+        if(inputArray[i] <= inputArray[i-1]){
+            let diff:number = inputArray[i-1] - inputArray[i] + 1;
+            inputArray[i] += diff;
+            cnt += diff;
+        }
+    }
+    return cnt;
 }
 
 console.log(arrayChange([1, 1, 1]));
