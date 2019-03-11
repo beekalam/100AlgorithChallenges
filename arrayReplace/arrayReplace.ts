c99b856a2aadc67d460703df1d52eb92e6a0e56e
beekalam <beekalam@gmail.com>
2019-03-11 07:45:37 +0330

    arrayreplace

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,5 +1,19 @@
 function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
-   
+   for(let i=0; i < inputArray.length; i++){
+       if(inputArray[i] == elemToReplace) 
+            inputArray[i] = substitutionElem;
+   }
+   return inputArray;
 }
 
+function arrayReplace2(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
+    inputArray.forEach((element, index) => {
+        if(element == elemToReplace){
+            inputArray[index] = substitutionElem;
+        }
+    });
+
+    return inputArray;
+ }
+ 
 console.log(arrayReplace([1, 2, 1], 1, 3));
