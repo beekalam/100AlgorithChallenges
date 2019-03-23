54c9350910cfd7103d846b8372114d90e72c13c9
beekalam <beekalam@gmail.com>
2019-03-23 09:28:43 +0430

    characterParity

--- characterParity/characterParity.ts
+++ characterParity/characterParity.ts
@@ -1,5 +1,10 @@
 function characterParity(symbol: string): string {
-   
+    let n = parseInt(symbol);
+    console.log(n);
+    if(isNaN(n)){
+        return "not a digit"
+    }
+    return n % 2 == 0 ? 'even':'odd';
 }
 
 console.log(characterParity('5'))
