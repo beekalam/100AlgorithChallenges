e4e4e5fa488fea1e84f524846265f9dad6cb7863
beekalam <beekalam@gmail.com>
2019-03-29 09:51:40 +0430

    circleofnumbers

--- circleOfNumbers/circleOfNumbers.ts
+++ circleOfNumbers/circleOfNumbers.ts
@@ -1,5 +1,23 @@
-function circleOfNumbers(n: number, firstNumber: number): number {
-
+function circleOfNumbers(n:number, firstNumber: number): number {
+    let ret = firstNumber + Math.floor(n/2);
+    return ret == n ? 0 : ret;
 }
 
+function circleOfNumbers2(n: number, firstNumber: number): number {
+    const numArray =[];
+    const halfway = n /2;
+    for(let i = 0; i < n; i++){
+        numArray.push(i);
+    }
+    console.log(numArray);
+
+    if(firstNumber < halfway){
+        return numArray[firstNumber + halfway];
+    }
+
+    return numArray[firstNumber - halfway];
+}
+
+
 console.log(circleOfNumbers(10, 2));
+console.log(circleOfNumbers(10, 5));
