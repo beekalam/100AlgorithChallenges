3e699bd32d478faba7a9fc196a4ccab48a95ddb0
beekalam <beekalam@gmail.com>
2019-04-01 11:21:08 +0430

    confirmEnding

--- confirmEnding/confirmEnding.ts
+++ confirmEnding/confirmEnding.ts
@@ -1,6 +1,17 @@
-function confirmEnding(str: string, target: string) {
-   
+function confirmEnding2(str: string, target: string) {
+   let lent = target.length;
+   let lens = str.length;
+   if(lent > lens){
+       return false;
+   }
+    return str.substr(lens - lent) == target;   
 }
+
+function confirmEnding(str: string, target: string) {
+    const start = str.length - target.length;
+
+    return str.substr(start) == target;
+ }
 
 console.log(confirmEnding("Abstraction", "action"));
 console.log(confirmEnding("Open sesame", "pen"));
