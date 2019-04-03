b4ee5b3a0ec63fc357e3121d02852545ef2b0652
beekalam <beekalam@gmail.com>
2019-04-03 08:00:24 +0430

    containsDuplicates

--- containsDuplicates/containsDuplicates.ts
+++ containsDuplicates/containsDuplicates.ts
@@ -1,5 +1,18 @@
 function containsDuplicates(a: number[]): boolean {
+    return  (new Set(a)).size == a.length;
+}
 
+function containsDuplicates2(a: number[]): boolean {
+    a.sort( (x,y) =>{
+        return x - y;
+    });
+    for(let i = 1; i < a.length; i++){
+        if(a[i] == a[i-1]){
+            return false;
+        }
+    }
+
+    return true;
 }
 
 console.log(containsDuplicates([1, 2, 3, 1]));
