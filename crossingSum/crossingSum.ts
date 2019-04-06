d1f02331b1c8bcdf05d3fc5d9d8760e3736c4e82
beekalam <beekalam@gmail.com>
2019-04-06 08:31:49 +0430

    crossingsum

--- crossingSum/crossingSum.ts
+++ crossingSum/crossingSum.ts
@@ -1,6 +1,26 @@
-function crossingSum(matrix: number[][], a: number, b: number): number {
-  
+function crossingSum2(matrix: number[][], a: number, b: number): number {
+    let row = matrix[a];
+    
+    for(let j = 0; j < matrix.length; j++){
+        if(j != a){
+            row.push(matrix[j][a])
+        }
+    }
+    
+    return row.reduce((p:number,c:number) => p + c, 0);
 }
+
+function crossingSum(matrix: number[][], a: number, b: number): number {
+    const columnTotal= matrix[a].reduce((a,b) => a + b);
+    let rowTotal= 0;
+
+    for(let i = 0; i < matrix.length; i++){
+        rowTotal += i !== a  ? matrix[i][b] : 0;
+    }
+
+    return columnTotal + rowTotal;
+}
+
 
 console.log(crossingSum([[1, 1, 1, 1], 
     [2, 2, 2, 2], 
